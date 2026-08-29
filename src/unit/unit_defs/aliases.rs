1f2c79d6e3a9671ec7108cacd5426ca9e0564a39
janKaje <66396673+janKaje@users.noreply.github.com>
2026-08-28 23:44:07 -0500

    remove auto generated unit files from .gitignore

---
+++ src/unit/unit_defs/aliases.rs
@@ -0,0 +1,33 @@
+/// This file was generated automatically by the build script. 
+/// If you want to add units, edit `unit_definitions\derived_units.toml`
+/// If you want to change file layout, edit `build.rs`
+
+pub(crate) const UNIT_ALIASES: &[(&'static str, &'static str)] = &[
+    ("metre", "meter"),
+    ("deg", "degree (angle)"),
+    ("degree", "degree (angle)"),
+    ("da", "day"),
+    ("tropical year", "year"),
+    ("solar year", "year"),
+    ("common year", "calendar year"),
+    ("AU", "astronomical unit"),
+    ("micron", "micrometer"),
+    ("cc", "milliliter"),
+    ("deg C", "degree Celsius"),
+    ("deg F", "degree Fahrenheit"),
+    ("deg c", "degree Celsius"),
+    ("deg f", "degree Fahrenheit"),
+    ("rankine", "Rankine"),
+    ("Celsius", "degree Celsius"),
+    ("Fahrenheit", "degree Fahrenheit"),
+    ("celsius", "degree Celsius"),
+    ("fahrenheit", "degree Fahrenheit"),
+    ("statute mile", "mile"),
+    ("firkin", "firkin (mass)"),
+    ("speed of light", "light speed"),
+    ("lightspeed", "light speed"),
+    ("arc minute", "minute (angle)"),
+    ("arc second", "second (angle)"),
+    ("arc degree", "degree (angle)"),
+    ("dalton", "unified atomic mass unit"),
+];
---
+++ src/unit/unit_defs/base_units.rs
@@ -0,0 +1,19 @@
+/// This file was generated automatically by the build script. 
+/// If you want to add units, edit `unit_definitions\base_units.toml`
+/// If you want to change file layout, edit `build.rs`
+
+#[allow(unused)]
+pub(crate) const NUMBER_OF_BASE_UNITS: u8 = 8;
+
+#[rustfmt::skip]
+pub(crate) const BASE_UNITS: &[(&'static str, &'static str, f64, f64, f64, f64, f64, f64, f64, f64, f64, f64)] = &[
+    ("USD", "USD", 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, ),
+    ("ampere", "A", 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, ),
+    ("candela", "cd", 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, ),
+    ("kelvin", "K", 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, ),
+    ("kilogram", "kg", 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, ),
+    ("meter", "m", 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, ),
+    ("mole", "mol", 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, ),
+    ("second", "s", 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, ),
+];
+
