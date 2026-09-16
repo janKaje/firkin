56eeaa25fd0504d5011b3c6a095889077a727851
janKaje <66396673+janKaje@users.noreply.github.com>
2026-09-16 19:40:53 +0100

    add semitone

--- python/firkin/units/log_units.py
+++ python/firkin/units/log_units.py
@@ -3,3 +3,4 @@ from firkin import LogFirkin
 bel = LogFirkin.unit("bel")
 decibel = LogFirkin.unit("decibel")
 neper = LogFirkin.unit("neper")
+semitone = LogFirkin.unit("semitone")
--- src/unit/unit_defs/log_units.rs
+++ src/unit/unit_defs/log_units.rs
@@ -7,4 +7,5 @@ pub(crate) const LOG_UNITS: &[(&'static str, &'static str, f64)] = &[
     ("bel", "B", 10.0),
     ("decibel", "dB", 1.2589254117941673),
     ("neper", "Np", 2.718281828459045),
+    ("semitone", "stn", 1.0594630943592953),
 ];
